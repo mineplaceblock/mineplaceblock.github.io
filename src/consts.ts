5bd2d96bbde0c2dfbad7b29a407479106440e76d
mineplaceblock <mineplaceblock@gmail.com>
2026-02-09 12:03:46 +0000

    Cambiando la pagina principal y titulos

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog de Aleix i Nil';
+export const SITE_DESCRIPTION = "Benvingut a la nostra pàgina web. Estem agraïts que hagis acabat aquí i esperem que els nostres posts t’agradin.";
